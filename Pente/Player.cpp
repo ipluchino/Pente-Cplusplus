73516554936cc141caf84c22845ad7b123e49951
ipluchino <ianlikesmath@gmail.com>
2023-09-11 18:39:37 -0400

    Minor Comment Fix

--- Pente/Player.cpp
+++ Pente/Player.cpp
@@ -5,13 +5,6 @@ Player::Player() : m_color('W'), m_score(0), m_capturedPairs(0)
 {
 }
 
-/*
-//Parametric Constructor
-Player::Player(char a_color, unsigned int a_score, unsigned int a_capturedPairs) : m_color(a_color), m_score(a_score), m_capturedPairs(a_capturedPairs)
-{
-}
-*/
-
 //Virtual function - It has its own definition in both the Human and Computer class.
 void Player::MakePlay(Board& a_board)
 {
