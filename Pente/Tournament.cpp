294e310ce18b305537c66935dc718df4d6230771
ipluchino <ianlikesmath@gmail.com>
2023-09-20 22:11:44 -0400

    Comments

--- Pente/Tournament.cpp
+++ Pente/Tournament.cpp
@@ -1,24 +1,48 @@
 #include "Tournament.h"
 
-//Default Constructor
+/* *********************************************************************
+Function Name: Tournament
+Purpose: To construct a tournament object.
+Parameters: None
+Return Value: None
+Algorithm: None
+Assistance Received: None
+********************************************************************* */
 Tournament::Tournament()
 {
 }
 
-//Loads the tournament from a file.
+/* *********************************************************************
+Function Name: LoadGame
+Purpose: To load a tournament from a file.
+Parameters: None
+Return Value: Whether or not the tournament was successfully loaded from the file, a boolean value.
+Algorithm: None
+Assistance Received: None
+********************************************************************* */
 bool Tournament::LoadGame()
 {
 	bool successfullyLoaded = m_round.LoadGameData();
 	return successfullyLoaded;
 }
 
-//Main game 
+/* *********************************************************************
+Function Name: StartTournament
+Purpose: To start a Pente tournament
+Parameters: None
+Return Value: None
+Algorithm:
+			1) Ask the user if they would like to start a new tournament or load one from a file.
+			2) Run a single round of the new or loaded tournament.
+			3) On round completion, ask the user if they would like to continue playing the tournament. If yes, run another round.
+			4) Display the winner of the tournament.
+Assistance Received: None
+********************************************************************* */
 void Tournament::StartTournament()
 {
 	cout << "Welcome to Pente!" << endl;
 
 	//If the user decides to load from a file, obtain the file name and load the game. Otherwise, start a new game.
-	//Continue to ask the user what they would like to do until they either start a new game or successfully load a game from a file.
 	bool success = false;
 	while (!success)
 	{
@@ -35,13 +59,29 @@ void Tournament::StartTournament()
 		}
 	}
 
+	//Continue running rounds of the tournament until the user wishes to stop.
 	do 
 	{
 		m_round.StartRound();
 
-	} while (Continue());		
+	} while (Continue());	
+
+	//Display the winner.
+	string winner = WhoWon();
+	cout << winner << endl;
+	cout << "Thanks for playing!" << endl;
 }
 
+/* *********************************************************************
+Function Name: Continue
+Purpose: To determine if the user would like to start another round of the tournament.
+Parameters: None
+Return Value: Whether or not the user would like to start another round, a boolean value.
+Algorithm:
+			1) Display the updated tournament scores of both the human and computer player.
+			2) Ask the user if they would like to start another round of Pente.
+Assistance Received: None
+********************************************************************* */
 bool Tournament::Continue()
 {
 	cout << "Human's Updated Tournament Score: " << m_round.GetHumanScore() << endl;
@@ -54,16 +94,21 @@ bool Tournament::Continue()
 		return true;
 	}
 	else
-	{
-		string winner = WhoWon();
-		
-		cout << winner << endl;
-		cout << "Thanks for playing!" << endl;
-		
+	{	
 		return false;
 	}
 }
 
+/* *********************************************************************
+Function Name: WhoWon
+Purpose: To determine who won the tournament.
+Parameters: None
+Return Value: The winner of the tournament, a string.
+Algorithm:
+			1) Obtain the scores of both the human and computer player.
+			2) Construct a string explaining the winner, and return it.
+Assistance Received: None
+********************************************************************* */
 string Tournament::WhoWon()
 {
 	int humanScore = m_round.GetHumanScore();
@@ -81,7 +126,7 @@ string Tournament::WhoWon()
 	}
 	else
 	{
-		winner = "The game has ended in a draw! Both players have the same amound of points";
+		winner = "The tournament has ended in a draw! Both players have the same amound of points.";
 	}
 
 	return winner;
